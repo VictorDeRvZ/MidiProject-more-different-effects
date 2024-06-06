e531457120310219e1df4ad0c750d7521d0f29ad
VictoryLap <Victorderoyvanzuydewijn@gmail.com>
2024-06-06 12:02:31 +0200

    First commit old project, multiple vfx should react to midi, but wanting to change to khz

---
+++ Source/FX_UE5/FX_UE5.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FX_UE5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FX_UE5, "FX_UE5" );
---
+++ Source/FX_UE5/Private/MyMIDIManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyMIDIManager.h"
+
+// Sets default values
+AMyMIDIManager::AMyMIDIManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyMIDIManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyMIDIManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
